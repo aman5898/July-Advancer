ff7323c265ee4312ca31c8abbbc18bd71c496964
Aman Kumar <amanku@microsoft.com>
2021-08-23 20:44:59 +0530

    Array Reverse

--- Arrays.java
+++ Arrays.java
@@ -8,6 +8,8 @@ public class Arrays {
         // for(int i=0;i<10;i++){
         //     System.out.println(marks[i]);
         // }
+
+
         Scanner scn = new Scanner(System.in);
         // mark = scn.nextInt();
         // System.out.println(mark);
@@ -17,8 +19,17 @@ public class Arrays {
         int[] arr = {1,2,3,4,5};
         // System.out.println(arr[5]);
 
+        // printing array
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
         }
+
+        System.out.println();
+        
+        // Printing reverse
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.print(arr[i]+" ");
+        }
+
     }
 }

20a3bd5d13f48ea44b502323b0d1b0cf3c170b46
Aman Kumar <amanku@microsoft.com>
2021-08-27 20:22:25 +0530

    Print First diagonal

--- Arrays.java
+++ Arrays.java
@@ -84,7 +84,17 @@ public class Arrays {
 
         int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
 
+        int sum = 0;
         // sum of first diagonal
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                if(i==j){
+                    sum+= arr[i][j];
+                }
+            }
+        }
+
+        System.out.println(sum);
 
     }
 

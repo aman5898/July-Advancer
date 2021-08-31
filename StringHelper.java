8f98743cbe8393526d91e5af67158b14b90ce86e
Aman Kumar <amanku@microsoft.com>
2021-08-31 21:49:43 +0530

    isPalindrom String

--- StringHelper.java
+++ StringHelper.java
@@ -15,7 +15,11 @@ public class StringHelper {
 
         System.out.println(str.indexOf("ee"));
 
-        printChars("geekster");
+        // printChars("geekster");
+
+        printSubstrings("geekster");
+
+        System.out.println(isPalindrome("malayalam"));
 
     }
 
@@ -27,6 +31,24 @@ public class StringHelper {
     }
 
     public static void printSubstrings(String s) {
-        
+        for(int i=0;i<s.length();i++){
+            for(int j=i+1;j<=s.length();j++){
+                System.out.println(s.substring(i, j));
+            }
+        }
+    }
+
+    public static boolean isPalindrome(String s) {
+        int l=0;
+        int r=s.length()-1;
+        while(l<=r){
+            if(s.charAt(l)!=s.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+
+        return true;
     }
 }

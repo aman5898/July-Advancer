6c989b0029f3e9fac2d8d56d9052ca505270a03f
Aman Kumar <amanku@microsoft.com>
2021-09-13 21:31:34 +0530

    Added PowerBtr

--- Recursion.java
+++ Recursion.java
@@ -1,18 +1,65 @@
-public class Recursion{
-    public static void main(String[] args){
-        printIncreasing(5);
+public class Recursion {
+    public static void main(String[] args) {
+        // printIncreasing(5);
+        // printDecreasing(5);
+        // System.out.println(factorial(5));
+        // System.out.println(power(3,3));
+        System.out.println(powerBtr(3, 5));
     }
 
-    public static void printIncreasing(int n){
+    public static void printIncreasing(int n) {
         // base case
-        if(n==0){
+        if (n == 0) {
             return;
         }
 
         // this will work is my faith
-        printIncreasing(n-1);
+        printIncreasing(n - 1);
 
         System.out.println(n);
 
+    }
+
+    public static void printDecreasing(int n) {
+
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+
+        // This is my faith call
+        printDecreasing(n - 1);
+    }
+
+    public static int factorial(int n){
+
+        if(n==0){
+            return 1;
+        }
+
+        // faith
+        int fnm1 = factorial(n-1);
+        return n*fnm1;
+    }
+
+    public static int power(int n, int k){
+        if(k==0){
+            return 1;
+        }
+        int nkm1 = power(n,k-1);
+
+        return nkm1*n;
+    }
+
+    public static int powerBtr(int n, int k){
+        if(k==0){
+            return 1;
+        }
+        int pdb2 = powerBtr(n,k/2);
+        if(k%2==0){
+            return pdb2*pdb2;
+        }else{
+            return pdb2*pdb2*n;
+        }
     }
 }
